f1ea991ecc122f9bc58316553facdaa82fe8287f
Dawid <dawid130892@interia.pl>
2023-07-20 20:31:55 +0200

    added more content

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const colors = document.querySelector(".colors");
 const colorsBtn = document.querySelector(".colors-btn");
 const colorsBtn2 = document.querySelector(".btn");
+const sizing = document.querySelector(".sizing");
+const sizingBtn = document.querySelector(".sizing-btn");
 
 function showadd() {
 	colors.classList.toggle("show");
 }
 colorsBtn.addEventListener("click", showadd);
+
+function sizingVisible() {
+	sizing.classList.toggle("show");
+}
+sizingBtn.addEventListener("click", sizingVisible);
