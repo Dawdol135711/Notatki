b1efcc6fc30cfd208ad4cf57c3f882d91355c5ac
Dawid <dawid130892@interia.pl>
2023-07-22 17:05:23 +0200

    Added function for calculating input value *2.36

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+// Functionality for showing notes
 const colors = document.querySelector(".colors");
 const colorsBtn = document.querySelector(".colors-btn");
 const colorsBtn2 = document.querySelector(".btn");
 const sizing = document.querySelector(".sizing");
 const sizingBtn = document.querySelector(".sizing-btn");
-const conect = document.querySelector(".conect")
-const conectBtn = document.querySelector('.conect-btn')
+const conect = document.querySelector(".conect");
+const conectBtn = document.querySelector(".conect-btn");
 
 function showadd() {
 	colors.classList.toggle("show");
@@ -21,3 +22,36 @@ function conectVisible() {
 }
 conectBtn.addEventListener("click", conectVisible);
 
+// Functionality for calculation *2,36
+const inputs = document.querySelectorAll(".calcInput");
+const calc = document.querySelector(".calc");
+function calcShow() {
+	inputs.forEach(element => {
+		element.classList.toggle("show");
+	});
+}
+calc.addEventListener("click", calcShow);
+// const calcValue = document.querySelector(".in")
+// const equals = document.querySelector(".out")
+// const value = calcValue.value
+
+// function debug(){
+// 	console.log(value);
+// 	equals.value = value;
+// }
+// calcValue.addEventListener("input", debug)
+
+const calcValue = document.querySelector(".in");
+const equals = document.querySelector(".out");
+
+function debug() {
+  const value = parseFloat(calcValue.value);
+
+  if (!isNaN(value)) {
+    equals.value = value*2.36;
+  } else {
+    equals.value = "Wprowadź liczbę!";
+  }
+}
+
+calcValue.addEventListener("input", debug);
