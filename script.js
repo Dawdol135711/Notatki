673a9693325f7df4f026f0198a49a975a3dfbfdb
Dawid <dawid130892@interia.pl>
2023-07-22 16:04:15 +0200

    Added some functionality

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const colorsBtn = document.querySelector(".colors-btn");
 const colorsBtn2 = document.querySelector(".btn");
 const sizing = document.querySelector(".sizing");
 const sizingBtn = document.querySelector(".sizing-btn");
+const conect = document.querySelector(".conect")
+const conectBtn = document.querySelector('.conect-btn')
 
 function showadd() {
 	colors.classList.toggle("show");
@@ -13,3 +15,9 @@ function sizingVisible() {
 	sizing.classList.toggle("show");
 }
 sizingBtn.addEventListener("click", sizingVisible);
+
+function conectVisible() {
+	conect.classList.toggle("show");
+}
+conectBtn.addEventListener("click", conectVisible);
+
